55386f222cab578006cdeaaa9f01aca69b0f75dd
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-07-20 06:04:12 -0300

    TinyHtmlTimeInput --> seconds support added to dateToString.

--- src/v1/libs/TinyHtml/input/date/TinyHtmlTimeInput.mjs
+++ src/v1/libs/TinyHtml/input/date/TinyHtmlTimeInput.mjs
@@ -36,8 +36,10 @@ class TinyHtmlTimeInput extends TinyHtmlInput {
 
     const hours = String(value.getHours()).padStart(2, '0');
     const minutes = String(value.getMinutes()).padStart(2, '0');
+    const seconds = String(value.getSeconds()).padStart(2, '0');
+    const milliSecondss = String(value.getMilliseconds()).padStart(3, '0');
 
-    return `${hours}:${minutes}`;
+    return `${hours}:${minutes}:${seconds}.${milliSecondss}`;
   }
 
   /**
