3dc871970bb4e5b256d26535d95dee5952979883
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-08-31 08:36:39 -0300

    tiny fix.

--- src/v1/libs/tools/TinyHttpResponseRegistry/MozillaRequestCodes/index.mjs
+++ src/v1/libs/tools/TinyHttpResponseRegistry/MozillaRequestCodes/index.mjs
@@ -1,8 +1,13 @@
+/*!
+ * MozillaRequestCodes
+ * CC-BY-SA 2.5 Licensed
+ */
+
 /**
  * A collection of standard HTTP status codes and their corresponding names and descriptions.
  *
  * Credits: https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status
- * @license CC-BY-SA 2.5 - https://creativecommons.org/licenses/by-sa/2.5/deed.en
+ * @license CC-BY-SA-2.5 - https://creativecommons.org/licenses/by-sa/2.5/deed.en
  * @type {import('../JsDoc.mjs').HttpResponses}
  */
 const RequestCodes = Object.freeze({
