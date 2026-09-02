e0d0a787bd45df5f826f7aadcab711c25037822e
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-02 04:15:27 -0300

    LoginRegexTemplates --> jsDoc updated.

--- src/v1/regexp/LoginRegexTemplates.mjs
+++ src/v1/regexp/LoginRegexTemplates.mjs
@@ -1,9 +1,18 @@
+/**
+ * @typedef {Object} UsernameRegexOptions
+ * @property {string} validValues The allowed characters for the username part.
+ * @property {string} prefix An prefix like '@' or '#'.
+ * @property {string} domainPattern A regex pattern for a domain.
+ */
+
 export const matrixProtocol = Object.freeze({
+  /** @type {UsernameRegexOptions} */
   userName: {
     prefix: '@',
     validValues: '[a-z0-9._=-]+',
     domainPattern: ':[a-z0-9.-]+\\.[a-z]{2,}',
   },
+  /** @type {UsernameRegexOptions} */
   roomName: {
     prefix: '#',
     validValues: '[a-z0-9._=-]+',
