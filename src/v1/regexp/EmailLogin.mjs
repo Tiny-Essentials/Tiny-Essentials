d6d57c45f5c5f3158c80a6be303cfb3b42247548
Yasmin Seidel (JasminDreasond) <jasmindreasond@proton.me>
2026-09-03 01:56:30 -0300

    EmailLogin regex --> script upgrade.

--- src/v1/regexp/EmailLogin.mjs
+++ src/v1/regexp/EmailLogin.mjs
@@ -78,23 +78,12 @@ export function emailRegex(options) {
 }
 
 /**
- * Validates whether a given string matches the specified email pattern, 
- * respects blacklist/whitelist rules, and passes a custom validator if provided.
  * @param {string} s - The string to validate.
  * @param {EmailRegexOptions} [options={}] - The configuration options.
  * @returns {boolean} True if the string is a valid email, false otherwise.
  * @throws {TypeError} If the input string is not a string.
  */
-export function isValidEmail(s, options = {}) {
-  if (typeof s !== 'string') {
-    throw new TypeError('The input must be a string.');
-  }
-
-  const pattern = emailRegex(options);
-  if (!pattern.test(s)) {
-    return false;
-  }
-
+function verifyEmail(s, options = {}) {
   // Runs custom validation if provided
   if (typeof options.customValidator === 'function' && !options.customValidator(s)) {
     return false;
@@ -123,6 +112,27 @@ export function isValidEmail(s, options = {}) {
 }
 
 /**
+ * Validates whether a given string matches the specified email pattern,
+ * respects blacklist/whitelist rules, and passes a custom validator if provided.
+ * @param {string} s - The string to validate.
+ * @param {EmailRegexOptions} [options={}] - The configuration options.
+ * @returns {boolean} True if the string is a valid email, false otherwise.
+ * @throws {TypeError} If the input string is not a string.
+ */
+export function isValidEmail(s, options = {}) {
+  if (typeof s !== 'string') {
+    throw new TypeError('The input must be a string.');
+  }
+
+  const pattern = emailRegex(options);
+  if (!pattern.test(s)) {
+    return false;
+  }
+
+  return verifyEmail(s, options);
+}
+
+/**
  * Generates a global RegExp object used to find all occurrences of URLs within a text.
  * @param {EmailRegexOptions} [options] - The configuration options.
  * @returns {RegExp} The resulting global RegExp object.
@@ -135,12 +145,12 @@ export function findEmailRegex(options) {
  * Extracts all matching email strings from a given text using the provided configuration.
  * @param {string} text - The text to search.
  * @param {EmailRegexOptions} [options] - The configuration options.
- * @returns {RegExpMatchArray | null} An array of matches or null if no matches are found.
+ * @returns {string[]} An array of matches or null if no matches are found.
  * @throws {TypeError} If the input text is not a string.
  */
 export function extractEmails(text, options) {
   if (typeof text !== 'string') {
     throw new TypeError('The input must be a string.');
   }
-  return text.match(findEmailRegex(options));
+  return text.match(findEmailRegex(options))?.filter((s) => verifyEmail(s, options)) ?? [];
 }
